916cb818420a003f41cc94b5733a92a389a4a215
yiseboge <Ethiopia1!>
2020-01-04 22:49:57 +0300

    clean up main

--- config/Config.go
+++ config/Config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"log"
 	"os"
@@ -32,4 +33,22 @@ func LoadConfig() Config {
 	}
 
 	return cfg
+}
+
+func SaveConfig(cfg Config) {
+	workingDirectory, _ := os.Getwd()
+	f, err := os.OpenFile(workingDirectory+"/config/config.yml", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	//x := io.Writer(f)
+
+	encoder := yaml.NewEncoder(f)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		fmt.Println("here")
+		log.Fatal(err)
+	}
+	defer encoder.Close()
 }
--- main.go
+++ main.go
@@ -80,39 +80,4 @@ func main() {
 		log.Printf("**Bad input, Please try again**")
 	}
 
-	//fmt.Println(models.DomainName{}.FindByName("www.google.com"))
-	//configuration := config.LoadConfig()
-
-	//fmt.Println("Start")
-
-	//var domain = models.DomainNameById(6)
-	//domain.Name = "trial Name"
-	//domain.UpdateDomainName()
-	//fmt.Println(models.DomainNamesByName("www.google.com"))
-
-	//var domain1 = models.DomainNameById(0)
-	//fmt.Println(domain1)
-	//var domain3 = models.DomainNameByName("www.google.com")
-	//fmt.Println(domain3)
-
-	//var domains = models.AllDomainNames()
-	//fmt.Println(domains)
-	//models.DeleteDomainName(domain1)
-	//fmt.Println(domain1)
-
-	//var domain1 = models.DomainNameById(1)
-	//fmt.Println(domain1)
-	//models.DeleteDomainName(domain1)
-	//fmt.Println(models.AllDomainNames())
-
-	//var domain2 = models.DomainName{Name: "www.apple.com", Address:"8.8.8.8"}
-	//var domain2 = models.DomainName{}.FindById(30)
-	//if domain2.ID > 0{
-	//	domain2.Address = "1.2.3.4"
-	//	domain2.Save()
-	//}
-
-	//fmt.Println(models.DomainName{}.FindAll())
-	//fmt.Println("End")
-
 }
